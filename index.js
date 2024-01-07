4de49d2717e48b711b81ed4dc239e785ba38345d
Adinan Cenci <adinancenci@protonmail.com>
2024-01-07 17:57:58 +0000

    third commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./src/Http.js');
+import Request from './src/Request.js';
+import Route from './src/Route.js';
+import RouteCollection from './src/RouteCollection.js';
+import TabLink from './src/TabLink.js';
+import TabManager from './src/TabManager.js';
+import TabPanel from './src/TabPanel.js';
+
+export default TabManager;
+export { Request, Route, RouteCollection, TabLink, TabPanel };
